c694e492d044527c257841404c39001817638cf0
elliehachemjob <elliehachemjob@gmail.com>
2021-12-06 14:19:04 +0200

    8th project done

---
+++ setup/src/SingleColor.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from "react";
+import rgbToHex from "./utils";
+
+const SingleColor = ({ rgb, weight, index, hexColor }) => {
+  const [alert, setAlert] = useState(false);
+  console.log(hexColor);
+  const bcg = rgb.join(",");
+  console.log(bcg);
+
+  const hex = rgbToHex(...rgb);
+  const hexValue = `#${hexColor}`;
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAlert(false);
+      return () => clearTimeout(timeout);
+    }, 3000);
+  }, [alert]);
+  return (
+    <article
+      className={`color ${index > 10 && "color-light"}`}
+      style={{ backgroundColor: `rgb(${bcg})` }}
+      onClick={() => {
+        setAlert(true);
+        navigator.clipboard.writeText(hexValue);
+      }}
+    >
+      <p className="percent-value">{weight}% </p>
+      <p className="color-value">{hexValue}</p>
+      {alert && <p className="alert"> Copied To Clipboard</p>}
+    </article>
+  );
+};
+
+export default SingleColor;
